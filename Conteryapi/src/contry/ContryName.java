566f6104c560dc0533653e661068a951a0e359d8
Gupta <akhilesh-mahendra.gupta@capgemini.com>
2022-02-21 17:32:50 +0530

    first java project added

---
+++ Conteryapi/src/contry/ContryName.java
@@ -0,0 +1,12 @@
+package contry;
+import java.util.Scanner;
+public class ContryName {
+
+	public static void main(String[] args) {
+Scanner sc =new Scanner(System.in);
+System.out.println("Enter the Country name:");
+String contry=sc.nextLine();
+System.out.println("the county name is:"+contry);
+	}
+
+}
