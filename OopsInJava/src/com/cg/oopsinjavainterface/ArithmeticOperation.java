ed439e9139fd7e27db05c32fb7f966e71c37127d
Gupta <akhilesh-mahendra.gupta@capgemini.com>
2022-03-04 00:12:22 +0530

    updated code

---
+++ OopsInJava/src/com/cg/oopsinjavainterface/ArithmeticOperation.java
@@ -0,0 +1,9 @@
+package com.cg.oopsinjavainterface;
+
+public interface ArithmeticOperation {
+static int a1=55;
+void add();
+void sub();
+void mul();
+void div();
+}
---
+++ StringsInJava/src/com/cgpatternprograming/NewPattern.java
@@ -0,0 +1,20 @@
+package com.cgpatternprograming;
+
+public class NewPattern {
+
+	public static void main(String[] args) {
+ for(int i=1;i<=3;i++)
+ {
+	 for(int j=8;j>6;j--)
+	 {
+		 System.out.print("/");
+		 System.out.println("");
+	 for(int k=1;k<6;k++)
+	 {
+		 System.out.print("*");
+	 }
+	 }
+ } System.out.println("");
+	}
+
+}
